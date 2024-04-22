43baa1f96d4d92c4ce4a270f1febed73ad0b18cb
EricAwesomeness <ericawesomeness@gmail.com>
2024-04-22 01:09:17 -0400

    adding event

--- src/main/java/net/ericawesomeness/ecaveplus/event/Tremor.java
+++ src/main/java/net/ericawesomeness/ecaveplus/event/Tremor.java
@@ -1,5 +1,14 @@
 package net.ericawesomeness.ecaveplus.event;
 
+import net.minecraftforge.event.level.ExplosionEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
 public class Tremor {
-    
+
+
+
+    @SubscribeEvent
+    public static void onExplosion(ExplosionEvent explosion){
+        if (explosion.getExplosion().getPosition() ){}
+    }
 }

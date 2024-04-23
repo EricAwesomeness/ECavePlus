ba8d92baa4e7c06eaac8648ad3a12c31dadffadf
EricAwesomeness <ericawesomeness@gmail.com>
2024-04-23 00:04:10 -0400

    adding event

--- src/main/java/net/ericawesomeness/ecaveplus/event/Tremor.java
+++ src/main/java/net/ericawesomeness/ecaveplus/event/Tremor.java
@@ -1,18 +1,18 @@
 package net.ericawesomeness.ecaveplus.event;
 
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.event.level.ExplosionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.List;
 
 public class Tremor {
 
     @SubscribeEvent
     public static void onExplosion(ExplosionEvent explosion){
-        for (Player player : explosion.getLevel().getEntitiesOfClass(Player.class)){
-            if (explosion.getExplosion().getPosition() ){
-
-            }
-        }
+        int range = 5;
+        AABB box = new AABB(event.getExplosion().getPosition()).inflate(range);
+        List<Player> players = explosion.getLevel().getEntitiesOfClass(Player.class, box, player -> explosion.getExplosion().getPosition().distanceTo(player.position()) < range);
     }
 }
